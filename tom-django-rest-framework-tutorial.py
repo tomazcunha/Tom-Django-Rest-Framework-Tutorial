1196fc029b89875a15ef490f3f2d183bef23511b
Tomaz Cunha <tomazmcn@gmail.com>
2019-11-26 23:16:49 -0300

    add links

--- tom-django-rest-framework-tutorial.py
+++ tom-django-rest-framework-tutorial.py
@@ -1,6 +1,7 @@
 # tom-django-rest-framework-tutorial.py
 # tomazmcn@gmail.com
-# 
+# https://github.com/tomazcunha/Tom-Django-Rest-Framework-Tutorial
+# https://www.django-rest-framework.org/tutorial/quickstart/
 
 
 # ##############################################################################
